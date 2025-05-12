011a2e528000351983fbddb6c4ca1485c68a40a7
scottrepreneur <me@stwoh.com>
2025-05-12 16:50:32 -0500

    tweak config

--- packages/consumer/ponder.config.ts
+++ packages/consumer/ponder.config.ts
@@ -8,12 +8,33 @@ export default createConfig({
   },
   ...createHatsProtocol({
     networks: {
+      // mainnet: {
+      //   rpcUrl: process.env.MAINNET_HTTP_PROVIDER || '',
+      // },
+      // optimism: {
+      //   rpcUrl: process.env.OPTIMISM_HTTP_PROVIDER || '',
+      // },
+      // gnosis: {
+      //   rpcUrl: process.env.GNOSIS_HTTP_PROVIDER || '',
+      // },
+      // polygon: {
+      //   rpcUrl: process.env.POLYGON_HTTP_PROVIDER || '',
+      // },
+      // arbitrum: {
+      //   rpcUrl: process.env.ARBITRUM_HTTP_PROVIDER || '',
+      // },
+      // celo: {
+      //   rpcUrl: process.env.CELO_HTTP_PROVIDER || '',
+      // },
+
+      // testnets
       sepolia: {
-        rpcUrl: process.env.PONDER_RPC_URL_11155111 || '',
+        rpcUrl: process.env.SEPOLIA_HTTP_PROVIDER || '',
       },
       // baseSepolia: {
-      //   rpcUrl: process.env.PONDER_RPC_URL_84532,
+      //   rpcUrl: process.env.BASE_SEPOLIA_HTTP_PROVIDER || '',
       // },
+
     },
   }),
 });
